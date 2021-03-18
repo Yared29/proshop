005798d0c1e6ee835085efd98dd1c4f42607902b
yared29 <yaredtekile5@gmail.com>
2021-03-18 04:25:33 -0700

    Header, Footer added

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Col className='text-center py-3'> Copyright &copy; ProShop</Col>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
