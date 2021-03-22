099d6d9cc3b9a572cafdf7609659fb679a52c894
yared29 <yaredtekile5@gmail.com>
2021-03-21 23:02:20 -0700

    Product screen added

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+
+const HomeScreen = () => {
+  const [products, setProducts] = useState([])
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
+}
+
+export default HomeScreen
