de9b50545f3e3fc58f0c00db1e39e4f6442f210c
yared29 <yaredtekile5@gmail.com>
2021-05-06 18:28:13 +0300

    connected to mongo atlas

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
-const app = express()
-
 dotenv.config()
+
+connectDB()
+
+const app = express()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -24,5 +28,6 @@ app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   )
 )
