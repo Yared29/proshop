5dd79c93f51057acc6228db36394de92e0576fe3
yared29 <yaredtekile5@gmail.com>
2021-05-06 14:47:38 +0300

    before db connectivity

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
 const app = express()
+
+dotenv.config()
 
 app.get('/', (req, res) => {
   res.send('API is running...')
@@ -15,4 +19,10 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+  )
+)
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
+import axios from 'axios'
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
   const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [match])
+
   return (
     <>
       <h1>Latest Products</h1>
