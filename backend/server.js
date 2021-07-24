9774cd0bd71a7e2db4773eaebcdd197e5604c375
Yared Tekileselassie <yaredtekile5@gmail.com>
2021-07-24 13:04:40 -0700

    backend user authenticaion added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import bodyParser from 'body-parser'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
@@ -12,11 +14,14 @@ connectDB()
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('API is running...')
+	res.send('API is running...')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -24,9 +29,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
-      .yellow.bold
-  )
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+			.yellow.bold
+	)
 )
